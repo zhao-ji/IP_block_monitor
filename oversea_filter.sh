7308070ed16b58dfead16066948ba5aa54ca529f
p0f <i@minganci.org>
2016-03-29 10:10:39 +0800

    fix: start the crawl

--- oversea_filter.sh
+++ oversea_filter.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# https://scans.io
     # input file name
     # exclude list from file
     # use syn scan on port 80
@@ -25,8 +26,3 @@ def store_pkg(pkg):
 with open(environ["TODAY_SERVER"], "a") as r:
     sniff(store=0, iface="eth1", filter="dst host 101.200.190.85 and tcp dst port 55555", prn=store_pkg)
 ' &> /dev/null) &
-
-sudo nmap -iL world_ip_list.txt --excludefile china_ip_list/china_ip_list.txt -PS -n -e eth1 --source-port 55555 --max-retries 0 -T insane &> /dev/null
-head -2 test_file  | grep -o '[[:digit:]]\{,3\}\.[[:digit:]]\{,3\}\.[[:digit:]]\{,3\}\.[[:digit:]]\{,3\}'
-
-# https://scans.io
