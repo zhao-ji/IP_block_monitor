f81d0e76ff062799cffdf31d092b14b12dbc0262
p0f <i@minganci.org>
2016-05-07 10:13:51 +0800

    fix: make the block scan work

--- block_monitor.sh
+++ block_monitor.sh
@@ -2,39 +2,48 @@
 # utf8
 
 function tcp_fast_ping {
-    read ip
-	fuck=$(sudo hping3 --fast --count 50 --numeric -q --destport 80 --syn $ip 3>&1 1>/dev/null 2>&3)
+	fuck=$(sudo hping3 --fast --count 10 --numeric -q --destport 80 --syn $1 3>&1 1>/dev/null 2>&3)
 	lost_rate=$(echo $fuck|awk '{print $12}'|cut -d '%' -f 1)
 	if [ "$lost_rate" = "100" ]
 	then
-		echo $ip
-	# else
-	# 	echo $lost_rate
+		TODAY_BLOCK_RESULT=$(date +%y_%m_%d_80_record)
+		echo $2 $1 >> "/home/nightwish/block_scan/scan_log/$TODAY_BLOCK_RESULT"
 	fi
 }
 
 function detect {
-while read domain
-do
-	ip_list=$(dig @8.8.8.8 +short $domain)
+	domain=$1
+
+	# 查询DNS 获取网站IP 两次不成就跳过 域名别名跳过 只取点分十进制IP
+	ip_list=$(dig @8.8.8.8 +short $domain|grep -o '[.0-9]*')
 	if [ -z "$ip_list" ]
 	then
-		ip_list=$(dig @8.8.8.8 +short $domain)
+		ip_list=$(dig @8.8.8.8 +short $domain|grep -o '[.0-9]*')
 	fi
-	block_ip=$(echo $ip_list|grep -o '[.0-9]*'|head -n 1|tcp_fast_ping)
-	if [ -n "$block_ip" ]
+	if [ -z "$ip_list" ]
 	then
-		echo $domain $block_ip
+		return
 	fi
-done
+
+	for ip in $ip_list
+	do
+		tcp_fast_ping $ip $domain
+	done
 }
 
 function main {
-cat top-1m.csv|cut -d ',' -f 2|detect
+    export -f detect
+	export -f tcp_fast_ping
+	cat /home/nightwish/block_scan/top-1m.csv|cut -d ',' -f 2|xargs --max-lines=1 --max-procs=120 -I DOMAIN bash -c  'detect DOMAIN'
 }
 
 function unit_test {
-    echo "twitter.com"|detect
+    export -f detect
+	export -f tcp_fast_ping
+    cat /home/nightwish/block_scan/top-1m.csv|cut -d ',' -f 2|head -n 10|xargs --max-lines=1 --max-procs=10 -I DOMAIN bash -c  'detect DOMAIN'
 }
 
+date
 main
+date
+# unit_test
