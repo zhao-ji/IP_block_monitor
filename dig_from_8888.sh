cde22d69698ef76569166d55a3a7d25be17d6eaf
p0f <i@minganci.org>
2016-05-13 23:29:36 +0800

    fix: complete but not test

--- dig_from_8888.sh
+++ dig_from_8888.sh
@@ -2,7 +2,12 @@
 # utf8
 
 ALEXA_DOWNLOAD_URL="http://s3.amazonaws.com/alexa-static/top-1m.csv.zip"
-TODAY_RECORD="scan_log/$(date +%y_%m_%d_record)"
+ERROR_LOG="/home/nightwish/block_scan/scan_log/log_error"
+
+TODAY_RECORD="scan_log/$(date +%y_%m_%d_DNS_record)"
+TODAY_SEND_LIST="scan_log/$(date +%y_%m_%d_foreign_ip_list)"
+TODAY_RECIEVE_LIST="scan_log/$(date +%y_%m_%d_syn_ack_list)"
+TODAY_DIFF="scan_log/$(date +%y_%m_%d_block_ip)"
 
 # 从alexa下载每日更新的全球前1M域名
 pushd /home/nightwish/block_scan
@@ -36,8 +41,7 @@ def store(pkg):
 
 with open(environ["TODAY_RECORD"], "a") as r:
     sniff(store=0, filter="src host 8.8.8.8 and udp port 53 and udp port 10002", prn=store)
-' &> /home/nightwish/block_scan/scan_log/log_error ) &
-# /dev/null ) &
+' &> $ERROR_LOG ) &
 
 # 向GOOGLE DNS服务器查询A记录
 cut -d, -f2 top-1m.csv|sudo python -c '
@@ -55,11 +59,61 @@ for line in stdin:
         qd=DNSQR(qname=line.strip(), qtype=1),
     )
     send([dns_query, dns_query, dns_query])
-' &> /dev/null
+' &> $ERROR_LOG
+
+# 休息三分钟后杀掉上个后台任务
+# http://stackoverflow.com/questions/1624691/linux-kill-background-task
+sleep 3m
+sudo kill $!
+
+# 找出所有外国IP 移除IPV4中的保留地址
+comm -23 <(cat $TODAY_RECORD|cut -d ' ' -f 3|sort -u) <(gzip -cd china_ip.gz) \
+	| grep -v -f reserved_address_block_regex >> $TODAY_SEND_LIST
+
+# 打开监控 关注syn-ack或rst-ack的返回
+(sudo TODAY_RECIEVE_LIST=$TODAY_RECIEVE_LIST python -c '
+from os import environ
+
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
+from scapy.all import sniff
+from scapy.all import IP, TCP
+
+def store(pkg):
+    r.write("{flags} {address}\n".format(
+        flags=pkg[TCP].flags, address=pkg[IP].src))
+
+with open(environ["TODAY_RECIEVE_LIST"], "a") as r:
+    sniff(
+        store=0, prn=store,
+        filter="tcp src port 80 and tcp dst port 10003 and tcp[8:4]==10004 and (tcp[tcpflags]==18 or tcp[tcpflags]==20)",
+    )
+' &> $ERROR_LOG ) &
+
+# 同IP建立握手
+cat $TODAY_SEND_LIST|sudo python -c '
+from sys import stdin
+
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+
+from scapy.all import send
+from scapy.all import IP, TCP
+
+for line in stdin:
+    tcp_syn = IP(dst=line.strip())/TCP(
+        dport=80, sport=10003,
+        seq=10003, flags="S",
+    )
+    send([tcp_syn, tcp_syn, tcp_syn])
+' &> $ERROR_LOG
 
 # 休息三分钟后杀掉上个后台任务
 # http://stackoverflow.com/questions/1624691/linux-kill-background-task
 sleep 8m
 sudo kill $!
+
+comm -23 <(cat $TODAY_SEND_LIST|sort -u) <(cat $TODAY_RECIEVE_LIST|sort -u) > $TODAY_DIFF
 
 popd
