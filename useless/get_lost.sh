9ff2046d78fe1c8191734766c5aa19307fc71f36
yulin <byteswarning@gmail.com>
2016-05-18 13:05:53 +0800

    add: 消失的数据
        - 谷歌DNS未能解析的域名
        - 香港无法访问的IP
        - 被防火墙丢掉的目标IP、域名

--- useless/get_lost.sh
+++ useless/get_lost.sh
@@ -3,18 +3,22 @@
 # 所有香港也无法连通的IP
 # 所有被屏蔽域名排行
 
+export LC_ALL=C
+
 join -1 2 -2 1 \
-	<( cat ../top-1m.csv | sed s/,/ /g | sort ) \
+	<( cat ../top-1m.csv | sed 's/,/ /g' | sort -k 2 ) \
 	<( comm -23 \
 		<(cut -d, -f2 ../top-1m.csv|sort) \
 		<(cat ../scan_log/16_05_17_DNS_record|cut -d ' ' -f 1|sort) \
+        | sort \
 	) > lost_domain
 
-comm -23 <(cat 16_05_17_foreign_ip_list|sort) <(cat 16_05_17_hongkong|sort) > lost_ip
+comm -23 <(cat ../scan_log/16_05_17_foreign_ip_list|sort) <(cat ../scan_log/16_05_17_hongkong|sort) > lost_ip
 
 join -1 2 -2 1 \
-	<( cat ../top-1m.csv | sed s/,/ /g | sort ) \
-	<( join -j 1 \
-		<(cat ../scan_log/16_05_17_DNS_record | sort -u) \
+	<( cat ../top-1m.csv | sed 's/,/ /g' | sort -k 2) \
+	<( join -1 3 -2 1 \
+		<(cat ../scan_log/16_05_17_DNS_record | sort -k 3 -u) \
 		<(cat ../scan_log/16_05_17_diff | sort) \
-	) | sort -k 1 -n > block_domain
+        | awk '{print $2, $3, $1}' | sort | tee union_record \
+	) | sort -k 2 -n > block_domain
