66dde7ad9d84a71031c40fa9d00f6ea5764bc910
yulin <byteswarning@gmail.com>
2016-05-23 10:41:32 +0800

    fix: make result pretty

--- useless/get_lost.sh
+++ useless/get_lost.sh
@@ -9,16 +9,16 @@ join -1 2 -2 1 \
 	<( cat ../top-1m.csv | sed 's/,/ /g' | sort -k 2 ) \
 	<( comm -23 \
 		<(cut -d, -f2 ../top-1m.csv|sort) \
-		<(cat ../scan_log/16_05_17_DNS_record|cut -d ' ' -f 1|sort) \
+		<(cat ../scan_log/16_05_23_DNS_record|cut -d ' ' -f 1|sort) \
         | sort \
-	) > lost_domain
+	) | awk '{print $2, $1}' > lost_domain
 
-comm -23 <(cat ../scan_log/16_05_17_foreign_ip_list|sort) <(cat ../scan_log/16_05_17_hongkong|sort) > lost_ip
+comm -23 <(cat ../scan_log/16_05_23_foreign_ip_list|sort) <(cat ../scan_log/16_05_23_hongkong|sort) > lost_ip
 
 join -1 2 -2 1 \
 	<( cat ../top-1m.csv | sed 's/,/ /g' | sort -k 2) \
 	<( join -1 3 -2 1 \
-		<(cat ../scan_log/16_05_17_DNS_record | sort -k 3 -u) \
-		<(cat ../scan_log/16_05_17_diff | sort) \
-        | awk '{print $2, $3, $1}' | sort | tee union_record \
-	) | sort -k 2 -n > block_domain
+		<(cat ../scan_log/16_05_23_DNS_record | sort -k 3 -u) \
+		<(cat ../scan_log/16_05_23_diff | sort) \
+        | awk '{print $2, $3, $1}' | sort \
+	) | sort -k 2 -n | awk '{print $2, $1, $3, $4}' > block_domain
