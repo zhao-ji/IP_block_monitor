bcf1eef2ef59af810ad07120a512eeba46923cd3
yulin <byteswarning@gmail.com>
2016-05-25 10:22:58 +0800

    fix: make the scripts complex again

--- block_monitor.sh
+++ block_monitor.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 
+# 开启debug模式 排序按C模式
 set -x
 export LC_MESSAGES=C
 
 ALEXA_DOWNLOAD_URL="http://s3.amazonaws.com/alexa-static/top-1m.csv.zip"
-ERROR_LOG="scan_log/log_error"
 
-TODAY_RECORD="scan_log/$(date +%y_%m_%d_DNS_record)"
-TODAY_SEND_LIST="scan_log/$(date +%y_%m_%d_foreign_ip_list)"
-TODAY_RECIEVE_LIST="scan_log/$(date +%y_%m_%d_syn_ack_list)"
-TODAY_DIFF="scan_log/$(date +%y_%m_%d_diff)"
+ERROR_LOG="useless/log_error"
+
+DNS_RECORD="scan_log/$(date +%y_%m_%d_DNS_record)"
+SEND_HOST="scan_log/$(date +%y_%m_%d_foreign_ip_list)"
+RECV_HOST="scan_log/$(date +%y_%m_%d_syn_ack_list)"
+DIFF="scan_log/$(date +%y_%m_%d_diff)"
+HOST_LOST="scan_log/$(date +%y_%m_%d_lost)"
 
 pushd /home/nightwish/block_scan
+
+# 获取配置参数
 source .fuck_info
 
 # 从alexa下载每日更新的全球前1M域名
-rm top1m.zip top-1m.csv
 wget $ALEXA_DOWNLOAD_URL -e use_proxy=yes -e http_proxy=$PROXY_INSTANCE -O top1m.zip 2> /dev/null
-unzip top1m.zip
+rm useless/top-1m.csv
+unzip -d useless top1m.zip
+rm top1m.zip
 
-touch $TODAY_RECORD $TODAY_RECIEVE_LIST
+touch $DNS_RECORD $RECV_HOST
 
 # 打开监控 关注域名的返回
-(sudo python recieve_DNS_record.py 2> $ERROR_LOG >> $TODAY_RECORD)&
+(sudo python recieve_DNS_record.py 2> $ERROR_LOG >> $DNS_RECORD)&
 
 # 向GOOGLE DNS服务器查询A记录
-cut -d, -f2 top-1m.csv|sudo python send_DNS_request.py &> $ERROR_LOG
+cut -d, -f2 useless/top-1m.csv|sudo python send_DNS_request.py &> $ERROR_LOG
 
 # 休息五分钟后把没有解析结果的域名再查一遍
 sleep 5m
-comm -23 <(cut -d, -f2 top-1m.csv|sort) <(cut -d ' ' -f1 $TODAY_RECORD|sort -u) \
+comm -23 <(cut -d, -f2 useless/top-1m.csv|sort) \
+    <(cut -d ' ' -f1 $DNS_RECORD|sort -u) \
     | sudo python send_DNS_request.py &> $ERROR_LOG
 
 # 休息五分钟后杀掉上个后台任务
@@ -38,25 +45,31 @@ sleep 5m
 sudo kill $!
 
 # 找出所有外国IP 移除IPV4中的保留地址
-comm -23 <(cat $TODAY_RECORD|cut -d ' ' -f3|grep '^[0-9\.]\{7,15\}$'|sort -u) <(zcat china_ip.gz) \
-    |grep -v -f reserved_IP_block_regex|sort -u >> $TODAY_SEND_LIST
+comm -23 <(cat $DNS_RECORD|cut -d ' ' -f3|grep '^[0-9\.]\{7,15\}$'|sort -u) <(zcat china_ip.gz) \
+    |grep -v -f reserved_IP_block_regex|sort -u >> $SEND_HOST
 
 # 打开监控 关注syn-ack或rst-ack的返回
-(sudo python recieve_ACK_or_RST.py 2> $ERROR_LOG >> $TODAY_RECIEVE_LIST)&
+(sudo python recieve_ACK_or_RST.py 2> $ERROR_LOG >> $RECV_HOST)&
 
 # 同IP建立握手
-cat $TODAY_SEND_LIST|sudo python send_SYN_request.py &> $ERROR_LOG
+cat $SEND_HOST|sudo python send_SYN_request.py &> $ERROR_LOG
 
 # 休息八分钟后杀掉上个后台任务
 # http://stackoverflow.com/questions/1624691/linux-kill-background-task
 sleep 8m
 sudo kill $!
 
-comm -23 <(cat $TODAY_SEND_LIST) <(cut -d ' ' -f 2 $TODAY_RECIEVE_LIST|sort -u) > $TODAY_DIFF
+comm -23 <(cat $SEND_HOST) <(cut -d ' ' -f 2 $RECV_HOST|sort -u) > $DIFF
 
-scp -P $HONGKONG_PORT $TODAY_DIFF $HONGKONG_HOST:~/block_scan/$TODAY_DIFF
+scp -P $HONGKONG_PORT $DIFF $HONGKONG_HOST:~/block_scan/$DIFF
 ssh -p $HONGKONG_PORT $HONGKONG_HOST "cd block_scan; bash foriegn_alive_check.sh hongkong"
-# scp -P $SEATTLE_PORT $TODAY_DIFF $SEATTLE_HOST:~/block_scan/$TODAY_DIFF
-# ssh -p $SEATTLE_PORT $SEATTLE_HOST "cd block_scan; bash foriegn_alive_check.sh hongkong"
+
+join -1 2 -2 1 \
+	<( cat useless/top-1m.csv | sed 's/,/ /g' | sort -k 2) \
+	<( join -1 3 -2 1 \
+		<(cat $DNS_RECORD | sort -k 3 -u) \
+		<(cat $DIFF | sort) \
+        | awk '{print $2, $3, $1}' | sort \
+	) | sort -k 2 -n | awk '{print $2, $1, $3, $4}' > $HOST_LOST
 
 popd
--- foriegn_alive_check.sh
+++ foriegn_alive_check.sh
@@ -2,27 +2,27 @@
 
 STATION=$1
 
-TODAY_DIFF="scan_log/$(date +%y_%m_%d_diff)"
-TODAY_RECIEVE="scan_log/$(date +%y_%m_%d_recieve)"
-TODAY_RECORD="scan_log/$(date +%y_%m_%d_$STATION)"
+DIFF="scan_log/$(date +%y_%m_%d_diff)"
+RECIEVE="scan_log/$(date +%y_%m_%d_recieve)"
+RECORD="scan_log/$(date +%y_%m_%d_$STATION)"
 
 ERROR_LOG="scan_log/log_error"
 
-touch $TODAY_RECIEVE
+touch $RECIEVE
 
 # 打开监控 关注syn-ack或rst-ack的返回
-(sudo python recieve_ACK_or_RST.py 2> $ERROR_LOG >> $TODAY_RECIEVE)&
+(sudo python recieve_ACK_or_RST.py 2> $ERROR_LOG >> $RECIEVE)&
 
 # 同IP建立握手
-cat $TODAY_DIFF|grep '^[0-9\.]\{7,15\}$'|sudo python send_SYN_request.py &> $ERROR_LOG
+cat $DIFF|grep '^[0-9\.]\{7,15\}$'|sudo python send_SYN_request.py &> $ERROR_LOG
 
 # 休息三分钟后杀掉上个后台任务
 # http://stackoverflow.com/questions/1624691/linux-kill-background-task
 sleep 3m
 sudo kill $!
 
-cut -d ' ' -f 2 $TODAY_RECIEVE|grep '^[0-9\.]\{7,15\}$'|sort -V -u > $TODAY_RECORD
+cut -d ' ' -f 2 $RECIEVE|grep '^[0-9\.]\{7,15\}$'|sort -V -u > $RECORD
 
 source .fuck_info
 
-scp -P $BEIJING_PORT $TODAY_RECORD $BEIJING_HOST:~/block_scan/$TODAY_RECORD
+scp -P $BEIJING_PORT $RECORD $BEIJING_HOST:~/block_scan/$RECORD
